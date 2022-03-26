fb3797168637de78c1edf1b2c3900ca56970bcb3
tamjid10 <tamjidahammad10@gmail.com>
2022-03-26 16:18:28 +0600

    erorr fix

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+const url = process.env.URL;
+mongoose.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
+
+app.listen(3000);
